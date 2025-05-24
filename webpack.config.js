1206ffa3dbca09138575266840579efc64325128
Tuan Anh <tp.anhh7@gmail.com>
2025-05-24 23:06:03 +0300

    fix problems

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,38 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/js/index.js',
+  entry: "./src/js/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
     clean: true,
-    publicPath: '/'
+    publicPath: "/",
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
-    })
+      template: "./src/index.html",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif|ico)$/i,
-        type: 'asset/resource'
-      }
-    ]
+        type: "asset/resource",
+      },
+    ],
   },
   devServer: {
     static: {
-      directory: path.join(__dirname, 'dist')
+      directory: path.join(__dirname, "dist"),
     },
     compress: true,
     port: 9000,
     hot: true,
-    historyApiFallback: true
-  }
+    historyApiFallback: true,
+  },
 };
